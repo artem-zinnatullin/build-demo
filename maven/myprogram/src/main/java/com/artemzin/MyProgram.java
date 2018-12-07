b94fa496de20df9fb52bdf584e1f99b66da8b5ed
loaner <ceo@artemzin.com>
2018-12-07 11:30:57 +0300

    Add Maven example

---
+++ maven/myprogram/src/main/java/com/artemzin/MyProgram.java
@@ -0,0 +1,10 @@
+package com.artemzin;
+
+import com.artemzin.a.A;
+
+public class MyProgram {
+  public static void main(String[] args) {
+    new A().a();
+    System.out.println("Hello world!");
+  }
+}
